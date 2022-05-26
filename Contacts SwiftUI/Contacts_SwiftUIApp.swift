e04e79676127d816002beabf6a55dc5d6b2c6aec
George <backs159@icloud.com>
2022-05-26 22:59:32 +0500

    Initial Commit

---
+++ Contacts SwiftUI/Contacts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_SwiftUIApp.swift
+//  Contacts SwiftUI
+//
+//  Created by Георгий Кузнецов on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contacts SwiftUI
+//
+//  Created by Георгий Кузнецов on 26.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
